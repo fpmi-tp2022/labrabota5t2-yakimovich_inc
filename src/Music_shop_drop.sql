2abf758ceb9c298a83272d8f0e54c035b95fc0ca
julliettee <juliet@juliet.localdomain>
2022-04-04 10:15:36 +0300

    sql-script: generate create & drop scripts using vertabelo

---
+++ src/Music_shop_drop.sql
@@ -0,0 +1,21 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2022-04-04 06:18:05.07
+
+-- foreign keys
+
+
+
+
+
+
+-- tables
+DROP TABLE CompactDisk;
+
+DROP TABLE MusicalComposition;
+
+DROP TABLE Trade;
+
+DROP TABLE TradeCodeInfo;
+
+-- End of file.
+

8582eb5fa91387e5653cbe93873806f1bf7922be
julliettee <juliet@juliet.localdomain>
2022-04-05 08:26:45 +0300

    base-code: add authorization, open & close db

---
+++ src/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include < string.h >
+#include "sqlite3.h"
+#include "DB.h"
+
+#define TRUE 1
+#define FALSE 0
+
+
+#pragma warning(disable:4996)
+
+void Authorization() {
+	printf("Hello, who is there?\n");
+	printf("Enter your name:\n");
+	char name[10];
+	scanf("%s", name);
+	if (!strcmp(name, ownerLogin))  {
+		printf("Enter your password:\n");
+		char pass[10];
+		scanf("%s", pass);
+		int attempts = 0;
+		while (strcmp(pass, ownerPassword) && attempts < ATTEMPTS_AMOUNT) {
+			printf("Incorrect password, please try again:\n");
+			scanf("%s", pass);
+			attempts++;
+		}
+		if (!strcmp(pass, ownerPassword)) {
+			printf("\nYou have all access to data base.\n\n");
+			accessRights = OWNER_RIGHTS;
+			return;
+		}
+	}
+	printf("\nYou have access only for requests, marked with *. \n\n");
+	accessRights = CLIENT_RIGHTS;
+	return;
+}
+
+//Show menu and orginize work of system
+int main(int argc, char* argv[]) {
+
+	Authorization();
+
+	sqlite3* db;
+	int rc = sqlite3_open("Music_shop.db", &db);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return 1;
+	}
+
+	int go = TRUE;
+	while (go) {
+		go = FALSE;
+	}
+	sqlite3_close(db);
+}

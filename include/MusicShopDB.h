bcff96b69c0dcf546f3f6470c863da3d97850dcc
depravo <sorokaa6102@gmail.com>
2022-04-06 15:27:09 -0700

    Add Insert function

--- include/MusicShopDB.h
+++ include/MusicShopDB.h
@@ -23,3 +23,8 @@ void GetPeriodTrade(sqlite3* db, char* date1, char* date2);
 void GetCompactSoldInfo(sqlite3* db, int id);
 void Requests(sqlite3* db);
 int checkDate(char date[]);
+int Insert(sqlite3 *db, char *zErrMsg, int ret);
+static int callback2(void* NotUsed, int argc, char** argv, char** azColName);
+static int callback3(void* NotUsed, int argc, char** argv, char** azColName);
+char* GetLastDate(sqlite3* db);
+int HaveDisks(char *date, sqlite3* db);
